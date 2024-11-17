d5325fa0630252c2cc81693c36a193a58ed19e08
Woojin1123 <boom175839@gmail.com>
2024-11-17 17:10:43 +0900

    헤더 추가

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    <LoginHeader v-if="$route.meta.requiresHeader" />
     <!-- 현재 경로에 따른 컴포넌트를 렌더링 -->
     <RouterView />
   </div>
@@ -8,4 +8,5 @@
 
 <script setup>
 import { RouterView } from 'vue-router';
+import LoginHeader from '@/components/LoginHeader.vue'; // LoginHeader 경로
 </script>
