5ad48f98af2838c09ef731cd1bbb8f25a61294c2
ican0422 <ican0422@gmail.com>
2024-11-18 21:15:27 +0900

    Revert "fix: 깃 충돌 해결"
    This reverts commit f37b5e1a3a056de7c5549a88519e6a6ac1967e13, reversing
    changes made to 40481af8e2c3541ebb929b2f8066fd1f7e32db69.

--- src/main.js
+++ src/main.js
@@ -5,16 +5,13 @@ import {createPinia} from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import ElementPlus from 'element-plus'
-import "bootstrap/dist/css/bootstrap.css"
-import axios from '@/plugins/axios.js'
+import axios from 'axios'
 
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
 
 app.mount('#app')
 
