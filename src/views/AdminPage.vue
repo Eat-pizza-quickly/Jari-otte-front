df7600e13412aaf3261b7bdb72fb5a943384320d
Woojin1123 <boom175839@gmail.com>
2024-11-21 15:50:36 +0900

    url수정

--- src/views/AdminPage.vue
+++ src/views/AdminPage.vue
@@ -2,24 +2,45 @@
   <div class="admin-page">
     <h2>Admin Page</h2>
     <div class="admin-buttons">
-      <button @click="$router.push({ name: 'concertCreate' })" class="admin-button">Create Concert</button>
-      <button @click="$router.push({ name: 'venueCreate' })" class="admin-button">Create Venue</button>
-      <button @click="$router.push({ name: 'couponPage' })" class="admin-button">Create Coupon</button>
+      <button @click="navigateTo('concertCreate')" class="admin-button">Create Concert</button>
+      <button @click="navigateTo('venueCreate')" class="admin-button">Create Venue</button>
+      <button @click="navigateTo('couponPage')" class="admin-button">Create Coupon</button>
       <button @click="logout" class="admin-button logout-button">Logout</button>
     </div>
   </div>
 </template>
 
 <script>
+// 커스텀 axios 인스턴스 import
+import axios from '@/plugins/axios';
+
 export default {
   name: 'AdminPage',
   methods: {
     logout() {
-      // 'token'을 localStorage에서 제거하여 로그아웃 처리
-      localStorage.removeItem('token');
-      // 로그인 페이지로 리다이렉트
-      this.$router.push({ name: 'home' });
+      // 서버에 로그아웃 요청 전송
+      axios.post('/users/logout')
+        .then(() => {
+          // 'token'을 localStorage에서 제거하여 로그아웃 처리
+          localStorage.removeItem('token');
+          // 로그인 페이지로 리다이렉트
+          this.$router.push({ name: 'home' });
+        })
+        .catch(error => {
+          console.error('로그아웃 실패:', error);
+          // 에러 처리 (예: 사용자에게 에러 메시지 표시)
+        });
     },
+  },
+  created() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      // 토큰이 없으면 로그인 페이지로 리다이렉트
+      this.$router.push({ name: 'login' });
+    } else {
+      // 토큰이 있으면 axios 인스턴스에 토큰 설정
+      apiClient.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    }
   },
 };
 </script>
